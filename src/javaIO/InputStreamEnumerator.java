c1038d3b5d80ab6e6d3f378a26cff06522fb0965
Bartłomiej Świątek <emini1@swiatek.biz>
2016-09-19 21:02:46 +0200

    javaIO - Buffered, Sequence Input stream demo

---
+++ src/javaIO/InputStreamEnumerator.java
@@ -0,0 +1,28 @@
+package javaIO;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PushbackInputStream;
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class InputStreamEnumerator implements Enumeration<FileInputStream> {
+    private Enumeration<String> files;
+
+    public InputStreamEnumerator(Vector<String> files){
+        this.files = files.elements();
+    }
+
+    public boolean hasMoreElements(){
+        return files.hasMoreElements();
+    }
+
+    public FileInputStream nextElement(){
+        try{
+            return new FileInputStream(files.nextElement().toString());
+        } catch (IOException e){
+            return null;
+        }
+    }
+}
---
+++ src/javaIO/SequenceInputStreamDemo.java
@@ -0,0 +1,36 @@
+package javaIO;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class SequenceInputStreamDemo {
+    public static void main(String[] args) {
+        int c;
+        Vector<String> files = new Vector<String>();
+
+        files.addElement("data/file1.txt");
+        files.addElement("data/file2.txt");
+        files.addElement("data/file3.txt");
+        InputStreamEnumerator ise = new InputStreamEnumerator(files);
+        InputStream input = new SequenceInputStream(ise);
+
+        try{
+            while((c = input.read()) != -1)
+                System.out.print((char) c);
+        } catch (NullPointerException e){
+            System.out.println("blad otwierania pliku");
+        } catch (IOException e){
+            System.out.println("blad I/O");
+        } finally {
+            try{
+                input.close();
+            }catch (IOException e){
+                System.out.println("Blad zamykania");
+            }
+        }
+    }
+}
