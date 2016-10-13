beaf4aac8c8ae2fc50aec0b3b729402a93884f16
Bartłomiej Świątek <emini1@swiatek.biz>
2016-10-13 18:24:43 +0200

    javaIO - MappedChannelRead

---
+++ src/javaNIO/MappedChannelRead.java
@@ -0,0 +1,26 @@
+package javaNIO;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+
+public class MappedChannelRead {
+    public static void main(String[] args) {
+        try(FileChannel fChan = (FileChannel) Files.newByteChannel(Paths.get("data/test.txt"))){
+            long fSize = fChan.size();
+            MappedByteBuffer mBuf = fChan.map(FileChannel.MapMode.READ_ONLY, 0, fSize);
+            for(int i=0; i < fSize; i++)
+                System.out.print((char)mBuf.get());
+            System.out.println();
+        } catch (InvalidPathException e){
+            System.out.println("błąd ścieżki: " + e);
+        } catch (IOException e) {
+            System.out.println("błąd we/wy: " +e);
+        }
+    }
+}
