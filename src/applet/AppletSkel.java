baef8f55105c27f19e9348d7450c007be6baf84b
Bartłomiej Świątek <emini1@swiatek.biz>
2016-10-25 18:46:44 +0200

    applet - skeleton

---
+++ src/applet/AppletSkel.java
@@ -0,0 +1,25 @@
+package applet;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class AppletSkel extends Applet {
+    public void init() {
+    }
+
+    public void start(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    public void destroy(){
+
+    }
+
+    public void paint(Graphics g){
+        
+    }
+}
