dd7287cc2627d3088eb9d45a21aac8a88b807ec7
Bartłomiej Świątek <emini1@swiatek.biz>
2016-12-25 14:15:14 +0100

    Swing - actions

--- src/Swing/MenuDemo.java
+++ src/Swing/MenuDemo.java
@@ -2,19 +2,20 @@ package Swing;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class MenuDemo implements ActionListener {
 
     JLabel jlab;
+    JPopupMenu jpu;
+    DebugAction setAct;
+    DebugAction clearAct;
+    DebugAction resumeAct;
 
     MenuDemo() {
         JFrame jfrm = new JFrame("Prezentacja Menu");
-        jfrm.setLayout(new FlowLayout());
-        jfrm.setSize(220, 200);
+
+        jfrm.setSize(360, 200);
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         jlab = new JLabel();
@@ -83,9 +84,64 @@ public class MenuDemo implements ActionListener {
         jmiReset.addActionListener(this);
         jmiAbout.addActionListener(this);
 
-        jfrm.add(jlab);
+        jfrm.add(jlab, BorderLayout.CENTER);
         jfrm.setJMenuBar(jmb);
         jfrm.setVisible(true);
+
+        jpu = new JPopupMenu();
+        JMenuItem jmiCut = new JMenuItem("Wytnij");
+        JMenuItem jmiCopy = new JMenuItem("Kopiuj");
+        JMenuItem jmiPaste = new JMenuItem("Wklej");
+        jpu.add(jmiCut);
+        jpu.add(jmiCopy);
+        jpu.add(jmiPaste);
+        jfrm.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if(e.isPopupTrigger())
+                    jpu.show(e.getComponent(), e.getX(), e.getY());
+            }
+            public void mouseReleased(MouseEvent e) {
+                if(e.isPopupTrigger())
+                    jpu.show(e.getComponent(), e.getX(), e.getY());
+            }
+        });
+        jmiCut.addActionListener(this);
+        jmiCopy.addActionListener(this);
+        jmiPaste.addActionListener(this);
+
+        ImageIcon setIcon = new ImageIcon("/home/bartek/Pictures/image002.jpg");
+        ImageIcon clearIcon = new ImageIcon("/home/bartek/Pictures/image002.jpg");
+        ImageIcon resumeIcon = new ImageIcon("/home/bartek/Pictures/image002.jpg");
+
+        setAct = new DebugAction("Ustaw punkt wstrzymania", setIcon, KeyEvent.VK_U,
+                KeyEvent.VK_U, "Ustaw punkt wstrzymania");
+        clearAct = new DebugAction("Usuń punkt wstrzymania", clearIcon, KeyEvent.VK_S,
+                KeyEvent.VK_S, "Usuń punkt wstrzymania");
+        resumeAct = new DebugAction("Wznów", resumeIcon, KeyEvent.VK_W,
+                KeyEvent.VK_W, "Wznów");
+        clearAct.setEnabled(false);
+
+        JButton jbtnSet = new JButton(setAct);
+        JButton jbtnClear = new JButton(clearAct);
+        JButton jbtnResume = new JButton(resumeAct);
+
+        JToolBar jtb = new JToolBar("Breakpoints");
+        jtb.add(jbtnSet);
+        jtb.add(jbtnClear);
+        jtb.add(jbtnResume);
+
+        jfrm.add(jtb, BorderLayout.NORTH);
+
+        JMenu jmDebug = new JMenu("Uruchamianie");
+        JMenuItem jmiSetBP = new JMenuItem(setAct);
+        JMenuItem jmiClearBP = new JMenuItem(clearAct);
+        JMenuItem jmiResume = new JMenuItem(resumeAct);
+
+        jmDebug.add(jmiSetBP);
+        jmDebug.add(jmiClearBP);
+        jmDebug.add(jmiResume);
+        jmOptions.add(jmDebug);
     }
 
     @Override
@@ -104,5 +160,27 @@ public class MenuDemo implements ActionListener {
                 new MenuDemo();
             }
         });
+    }
+
+    class DebugAction extends AbstractAction {
+        public DebugAction(String name, Icon image, int mnem, int accel, String tTip) {
+            super(name, image);
+            putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(accel, InputEvent.CTRL_DOWN_MASK));
+            putValue(MNEMONIC_KEY, new Integer(mnem));
+            putValue(SHORT_DESCRIPTION, tTip);
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String comStr = e.getActionCommand();
+            jlab.setText("Wybrano " + comStr);
+
+            if(comStr.equals("Ustaw punkt wstrzymania")){
+                clearAct.setEnabled(true);
+                setAct.setEnabled(false);
+            } else if(comStr.equals("Usuń punkt wstrzymania")){
+                clearAct.setEnabled(false);
+                setAct.setEnabled(true);
+            }
+        }
     }
 }
